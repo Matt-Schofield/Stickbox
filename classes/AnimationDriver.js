802789a65f42304d64cec602b075080728a6f5c2
Matthew Schofield <matthew.schofield-5@student.manchester.ac.uk>
2022-12-12 12:55:48 +0000

    Seperated frame data and processing code

--- classes/AnimationDriver.js
+++ classes/AnimationDriver.js
@@ -8,61 +8,6 @@ class AnimationDriver {
         this.nodes = this.initNodes();
 
         this.frameCounter = 0;
-
-        this.IDLE_FRAMES = [
-            {
-                head: { x: 0.8, y: 1 },
-                kneck: { x: 0.7, y: 0.85 },
-                leftElbow: { x: 0.20, y: 0.7 },
-                rightElbow: { x: 0.95, y: 0.6 },
-                leftHand: { x: 0.2, y: 0.55 },
-                rightHand: { x: 1.25, y: 0.55 },
-                balls: { x: 0.5, y: 0.5 },
-                leftKnee: { x: 0.3, y: 0.25 },
-                rightKnee: { x: 0.80, y: 0.25 },
-                leftFoot: { x: 0, y: 0 },
-                rightFoot: { x: 0.7, y: 0 },
-            },
-            {
-                head: { x: 0.8, y: 0.97 },
-                kneck: { x: 0.7, y: 0.82 },
-                leftElbow: { x: 0.20, y: 0.67 },
-                rightElbow: { x: 0.95, y: 0.57 },
-                leftHand: { x: 0.23, y: 0.52 },
-                rightHand: { x: 1.25, y: 0.54 },
-                balls: { x: 0.5, y: 0.47 },
-                leftKnee: { x: 0.3, y: 0.22 },
-                rightKnee: { x: 0.83, y: 0.22 },
-                leftFoot: { x: 0, y: 0 },
-                rightFoot: { x: 0.7, y: 0 },
-            },
-            {
-                head: { x: 0.8, y: 0.95 },
-                kneck: { x: 0.7, y: 0.80 },
-                leftElbow: { x: 0.20, y: 0.65 },
-                rightElbow: { x: 0.95, y: 0.55 },
-                leftHand: { x: 0.25, y: 0.50 },
-                rightHand: { x: 1.25, y: 0.52 },
-                balls: { x: 0.5, y: 0.45 },
-                leftKnee: { x: 0.3, y: 0.20 },
-                rightKnee: { x: 0.85, y: 0.20 },
-                leftFoot: { x: 0, y: 0 },
-                rightFoot: { x: 0.7, y: 0 },
-            },
-            {
-                head: { x: 0.8, y: 0.97 },
-                kneck: { x: 0.7, y: 0.82 },
-                leftElbow: { x: 0.20, y: 0.67 },
-                rightElbow: { x: 0.95, y: 0.57 },
-                leftHand: { x: 0.23, y: 0.52 },
-                rightHand: { x: 1.25, y: 0.54 },
-                balls: { x: 0.5, y: 0.47 },
-                leftKnee: { x: 0.3, y: 0.22 },
-                rightKnee: { x: 0.83, y: 0.22 },
-                leftFoot: { x: 0, y: 0 },
-                rightFoot: { x: 0.7, y: 0 },
-            }
-        ];
     }
 
     initNodes() {
@@ -81,13 +26,13 @@ class AnimationDriver {
         };
     }
 
-    animateEntity(animation, x, y) {
+    animateEntity(animation, x, y, dir) {
         switch (animation) {
             case 0:
                 this.nodes = this.setStatic();
                 break;
             case 1:
-                this.nodes = this.setIdling();
+                this.nodes = this.cycleAnimationFrames(IDLE_FRAMES, 22);
                 break;
             case 2:
                 this.nodes = this.setStatic();
@@ -96,7 +41,7 @@ class AnimationDriver {
                 this.nodes = this.setStatic();
                 break;
             case 4:
-                this.nodes = this.setStatic();
+                this.nodes = this.cycleAnimationFrames(CROUCHING_FRAMES, 22);
                 break;
             case 5:
                 this.nodes = this.setStatic();
@@ -108,9 +53,21 @@ class AnimationDriver {
         // console.log(animation, x, y);
 
         for (const node in this.nodes) {
-            this.nodes[node].x = x + (this.entityWidth * this.nodes[node].x);
+            this.nodes[node].x = x + (this.entityWidth * this.nodes[node].x * dir);
             this.nodes[node].y = y - (this.entityHeight * this.nodes[node].y);
         }
+    }
+
+    cycleAnimationFrames(frames, speedFactor) {
+        // Loop frame counter
+        if (this.frameCounter > ((frames.length * speedFactor) - 1)) {
+            this.frameCounter = 0;
+        }
+
+        let frame = structuredClone(IDLE_FRAMES[Math.floor(this.frameCounter/speedFactor)]);
+        this.frameCounter += 1;
+
+        return frame;
     }
 
     setStatic() {
@@ -127,23 +84,5 @@ class AnimationDriver {
             leftFoot: { x: 0, y: 0 },
             rightFoot: { x: 0.7, y: 0 },
         };
-    }
-
-    setIdling() {
-        // Loop frame counter - can be own method
-        if (this.frameCounter > ((this.IDLE_FRAMES.length * 20) - 1)) {
-            this.frameCounter = 0;
-        }
-
-        let frame = structuredClone(this.IDLE_FRAMES[Math.floor(this.frameCounter/20)]);
-        this.frameCounter += 1;
-
-        return frame;
-    }
-
-    setJump() {
-        frames = [
-
-        ];
     }
 }
